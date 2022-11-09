796b247e36377de65859a0e7cf6293e10b0c0d19
Ryan Shea <ryanshea10@gmail.com>
2022-11-09 15:12:20 -0500

    server.py file with basic song retrieval

---
+++ server.py
@@ -0,0 +1,49 @@
+import os
+from typing import Tuple
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response
+
+app = Flask(__name__)
+
+DATABASEURI = "postgresql://ss6170:6400@34.75.94.195/proj1part2"
+
+engine = create_engine(DATABASEURI)
+
+@app.before_request
+def before_request():
+  try:
+    g.conn = engine.connect()
+  except:
+    print("uh oh, problem connecting to database")
+    import traceback; traceback.print_exc()
+    g.conn = None
+
+@app.route('/songs', methods=['GET'])
+def get_songs() -> Tuple[str, bool]:
+    req = request.get_json()
+    uid = req['uid']
+    db = g.conn.execute("SELECT S.song_name, L.song_id \
+                          FROM likes_song L, Songs S \
+                          WHERE L.user_id = %s AND S.song_id = L.song_id;", uid)
+
+    songs = []
+    for i in db:
+        songs.append(tuple(i))
+    return {'songs': songs}
+
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+
+  def run(debug, threaded, host, port):
+    HOST, PORT = host, port
+    print("running on %s:%d" % (HOST, PORT))
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+  run()
